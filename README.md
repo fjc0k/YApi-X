53a8cb18a22e58daa6f2e658451d46a33344f764
方剑成 <fjc0kb@gmail.com>
2021-05-20 15:43:27 +0800

    feat: 支持 webdav 扩展的 HTTP 请求方法 PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK

--- README.md
+++ README.md
@@ -7,7 +7,8 @@
 - 新增 `adminPassword` 配置项以设置管理员密码；
 - 项目图标支持自传图片；
 - 接口运行支持上传文件（[需安装插件 →](https://github.com/fjc0k/YApi-X/tree/master/chrome-extension#readme)）；
-- 请求参数设置下的 Headers 支持批量添加。
+- 请求参数设置下的 Headers 支持批量添加；
+- 支持 webdav 扩展的 HTTP 请求方法 PROPFIND, PROPPATCH, MKCOL, COPY, MOVE, LOCK, UNLOCK（[了解更多 →](http://www.webdav.org/specs/rfc2518.html)）。
 
 ---
 
--- client/containers/Project/Interface/InterfaceList/AddInterfaceForm.js
+++ client/containers/Project/Interface/InterfaceList/AddInterfaceForm.js
@@ -1,18 +1,17 @@
 import React, { PureComponent as Component } from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, Select, Button } from 'antd';
+import { Form, Input, Select, Button } from 'antd'
 
 import constants from '../../../../constants/variable.js'
 import { handleApiPath, nameLengthLimit } from '../../../../common.js'
-const HTTP_METHOD = constants.HTTP_METHOD;
-const HTTP_METHOD_KEYS = Object.keys(HTTP_METHOD);
+const HTTP_METHOD = constants.HTTP_METHOD
+const HTTP_METHOD_KEYS = Object.keys(HTTP_METHOD)
 
-const FormItem = Form.Item;
-const Option = Select.Option;
+const FormItem = Form.Item
+const Option = Select.Option
 function hasErrors(fieldsError) {
-  return Object.keys(fieldsError).some(field => fieldsError[field]);
+  return Object.keys(fieldsError).some(field => fieldsError[field])
 }
-
 
 class AddInterfaceForm extends Component {
   static propTypes = {
@@ -20,110 +19,111 @@ class AddInterfaceForm extends Component {
     onSubmit: PropTypes.func,
     onCancel: PropTypes.func,
     catid: PropTypes.number,
-    catdata: PropTypes.array
+    catdata: PropTypes.array,
   }
-  handleSubmit = (e) => {
-    e.preventDefault();
+  handleSubmit = e => {
+    e.preventDefault()
     this.props.form.validateFields((err, values) => {
       if (!err) {
         this.props.onSubmit(values, () => {
-          this.props.form.resetFields();
-        });
-
+          this.props.form.resetFields()
+        })
       }
-    });
+    })
   }
 
-  handlePath = (e) => {
+  handlePath = e => {
     let val = e.target.value
     this.props.form.setFieldsValue({
-      path: handleApiPath(val)
+      path: handleApiPath(val),
     })
   }
   render() {
-    const { getFieldDecorator, getFieldsError } = this.props.form;
+    const { getFieldDecorator, getFieldsError } = this.props.form
     const prefixSelector = getFieldDecorator('method', {
-      initialValue: 'GET'
+      initialValue: 'GET',
     })(
-      <Select style={{ width: 75 }}>
+      <Select style={{ width: 110 }}>
         {HTTP_METHOD_KEYS.map(item => {
-          return <Option key={item} value={item}>{item}</Option>
+          return (
+            <Option key={item} value={item}>
+              {item}
+            </Option>
+          )
         })}
-      </Select>
-      );
+      </Select>,
+    )
     const formItemLayout = {
       labelCol: {
         xs: { span: 24 },
-        sm: { span: 6 }
+        sm: { span: 6 },
       },
       wrapperCol: {
         xs: { span: 24 },
-        sm: { span: 14 }
-      }
-    };
-
+        sm: { span: 14 },
+      },
+    }
 
     return (
-
       <Form onSubmit={this.handleSubmit}>
-        <FormItem
-          {...formItemLayout}
-          label="接口分类"
-        >
+        <FormItem {...formItemLayout} label='接口分类'>
           {getFieldDecorator('catid', {
-            initialValue: this.props.catid ? this.props.catid + '' : this.props.catdata[0]._id + ''
+            initialValue: this.props.catid
+              ? this.props.catid + ''
+              : this.props.catdata[0]._id + '',
           })(
             <Select>
               {this.props.catdata.map(item => {
-                return <Option key={item._id} value={item._id + ""}>{item.name}</Option>
+                return (
+                  <Option key={item._id} value={item._id + ''}>
+                    {item.name}
+                  </Option>
+                )
               })}
-            </Select>
-            )}
+            </Select>,
+          )}
         </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="接口名称"
-        >
+        <FormItem {...formItemLayout} label='接口名称'>
           {getFieldDecorator('title', {
-            rules: nameLengthLimit('接口')
-          })(
-            <Input placeholder="接口名称" />
-            )}
+            rules: nameLengthLimit('接口'),
+          })(<Input placeholder='接口名称' />)}
         </FormItem>
 
-        <FormItem
-          {...formItemLayout}
-          label="接口路径"
-        >
+        <FormItem {...formItemLayout} label='接口路径'>
           {getFieldDecorator('path', {
-            rules: [{
-              required: true, message: '请输入接口路径!'
-            }]
+            rules: [
+              {
+                required: true,
+                message: '请输入接口路径!',
+              },
+            ],
           })(
-            <Input onBlur={this.handlePath} addonBefore={prefixSelector} placeholder="/path" />
-            )}
+            <Input
+              onBlur={this.handlePath}
+              addonBefore={prefixSelector}
+              placeholder='/path'
+            />,
+          )}
         </FormItem>
-        <FormItem
-          {...formItemLayout}
-          label="注"
-        >
-          <span style={{ color: "#929292" }}>详细的接口数据可以在编辑页面中添加</span>
+        <FormItem {...formItemLayout} label='注'>
+          <span style={{ color: '#929292' }}>
+            详细的接口数据可以在编辑页面中添加
+          </span>
         </FormItem>
-        <FormItem className="catModalfoot" wrapperCol={{ span: 24, offset: 8 }} >
-          <Button onClick={this.props.onCancel} style={{ marginRight: "10px" }}  >取消</Button>
+        <FormItem className='catModalfoot' wrapperCol={{ span: 24, offset: 8 }}>
+          <Button onClick={this.props.onCancel} style={{ marginRight: '10px' }}>
+            取消
+          </Button>
           <Button
-            type="primary"
-            htmlType="submit"
-            disabled={hasErrors(getFieldsError())}
-          >
+            type='primary'
+            htmlType='submit'
+            disabled={hasErrors(getFieldsError())}>
             提交
           </Button>
         </FormItem>
-
       </Form>
-
-    );
+    )
   }
 }
 
-export default Form.create()(AddInterfaceForm);
+export default Form.create()(AddInterfaceForm)
